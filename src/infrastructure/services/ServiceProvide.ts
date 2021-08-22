f9babff3225270361ec8c6b0a401dd2a015ccb2e
Mathiew82 <amateo1982@yahoo.es>
2021-08-22 22:09:49 +0200

    Modificado el constructor del ServiceProvide. Ahora las dependencias se inyectan en el constructor

--- src/infrastructure/services/ServiceProvide.ts
+++ src/infrastructure/services/ServiceProvide.ts
@@ -1,9 +1,5 @@
 import { ConfigServiceProvide } from "../../domain/types/ConfigServiceProvide";
 
 export class ServiceProvide {
-  config: ConfigServiceProvide;
-
-  constructor(config: ConfigServiceProvide) {
-    this.config = config;
-  }
+  constructor(public config: ConfigServiceProvide) {}
 }
